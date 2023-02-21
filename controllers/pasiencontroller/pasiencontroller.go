5b1cc91d8fdb93c54c660f36712a47b938c30f88
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-21 22:12:10 +0700

    feat: add Edit data and field input

--- controllers/pasiencontroller/pasiencontroller.go
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -3,10 +3,11 @@ package pasiencontroller
 import (
 	"html/template"
 	"net/http"
+	"strconv"
 
 	"github.com/jeypc/go-crud/entities"
-	"github.com/jeypc/go-crud/models"
 	"github.com/jeypc/go-crud/libraries"
+	"github.com/jeypc/go-crud/models"
 )
 
 var pasienModel = models.NewPasienModel()
@@ -64,9 +65,49 @@ func Add(response http.ResponseWriter, request *http.Request) {
 }
 
 func Edit(response http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		queryString := request.URL.Query()
+		id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
 
+		var pasien entities.Pasien
+		pasienModel.Find(id, &pasien)
+
+		data := map[string]interface{}{
+			"pasien": pasien,
+		}
+
+		temp, err := template.ParseFiles("views/pasien/edit.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, data)
+	}	else if request.Method == http.MethodPost{
+		request.ParseForm()
+		
+		var pasien entities.Pasien
+		pasien.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64	)
+		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+		pasien.NIK = request.Form.Get("nik")
+		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+		pasien.Alamat = request.Form.Get("alamat")
+		pasien.NoHp = request.Form.Get("no_hp")
+
+		var data = make(map[string]interface{})
+
+		vErrors := validation.Struct(pasien)
+
+		if vErrors != nil{
+			data["validation"] = vErrors
+			data["pasien"] = pasien
+		} else{
+			data["pesan"] = "Data pasien berhasil diperbarui"
+			pasienModel.Update(pasien)
+		}
+		
+		temp, _ := template.ParseFiles("views/pasien/edit.html")
+		temp.Execute(response, data)
+	}
 }
 
-func Delete(response http.ResponseWriter, request *http.Request) {
-
-}
