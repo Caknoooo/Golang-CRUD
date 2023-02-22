5394da168bbff9c1a0d5e153929a7867ee907a66
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-02-22 18:28:45 +0700

    feat: remove unused code

--- controllers/pasiencontroller/pasiencontroller.go
+++ controllers/pasiencontroller/pasiencontroller.go
@@ -118,3 +118,186 @@ func Delete(response http.ResponseWriter, request *http.Request) {
 	pasienModel.Delete(id)
 	http.Redirect(response, request, "/pasien", http.StatusSeeOther)
 }
+
+
+
+// func Edit(response http.ResponseWriter, request *http.Request) {
+// 	if request.Method == http.MethodGet {
+// 		queryString := request.URL.Query()
+// 		id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+
+// 		var pasien entities.Pasien
+// 		pasienModel.Find(id, &pasien)
+
+// 		data := map[string]interface{}{
+// 			"pasien": pasien,
+// 		}
+
+// 		temp, err := template.ParseFiles("views/pasien/edit.html")
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		temp.Execute(response, data)
+// 	}	else if request.Method == http.MethodPost{
+// 		request.ParseForm()
+		
+// 		var pasien entities.Pasien
+// 		pasien.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64	)
+// 		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+// 		pasien.NIK = request.Form.Get("nik")
+// 		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+// 		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+// 		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+// 		pasien.Alamat = request.Form.Get("alamat")
+// 		pasien.NoHp = request.Form.Get("no_hp")
+
+// 		var data = make(map[string]interface{})
+
+// 		vErrors := validation.Struct(pasien)
+
+// 		if vErrors != nil{
+// 			data["validation"] = vErrors
+// 			data["pasien"] = pasien
+// 		} else{
+// 			data["pesan"] = "Data pasien berhasil diperbarui"
+// 			pasienModel.Update(pasien)
+// 		}
+		
+// 		temp, _ := template.ParseFiles("views/pasien/edit.html")
+// 		temp.Execute(response, data)
+// 	}
+// }
+
+// func Delete(response http.ResponseWriter, request *http.Request) {
+// 	queryString := request.URL.Query()
+// 	id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+	
+// 	pasienModel.Delete(id)
+// 	http.Redirect(response, request, "/pasien", http.StatusSeeOther)
+// }
+
+
+
+// func Edit(response http.ResponseWriter, request *http.Request) {
+// 	if request.Method == http.MethodGet {
+// 		queryString := request.URL.Query()
+// 		id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+
+// 		var pasien entities.Pasien
+// 		pasienModel.Find(id, &pasien)
+
+// 		data := map[string]interface{}{
+// 			"pasien": pasien,
+// 		}
+
+// 		temp, err := template.ParseFiles("views/pasien/edit.html")
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		temp.Execute(response, data)
+// 	}	else if request.Method == http.MethodPost{
+// 		request.ParseForm()
+		
+// 		var pasien entities.Pasien
+// 		pasien.Id, _ = strconv.ParseInt(request.Form.Get("id"), 10, 64	)
+// 		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+// 		pasien.NIK = request.Form.Get("nik")
+// 		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+// 		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+// 		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+// 		pasien.Alamat = request.Form.Get("alamat")
+// 		pasien.NoHp = request.Form.Get("no_hp")
+
+// 		var data = make(map[string]interface{})
+
+// 		vErrors := validation.Struct(pasien)
+
+// 		if vErrors != nil{
+// 			data["validation"] = vErrors
+// 			data["pasien"] = pasien
+// 		} else{
+// 			data["pesan"] = "Data pasien berhasil diperbarui"
+// 			pasienModel.Update(pasien)
+// 		}
+		
+// 		temp, _ := template.ParseFiles("views/pasien/edit.html")
+// 		temp.Execute(response, data)
+// 	}
+// }
+
+// func Delete(response http.ResponseWriter, request *http.Request) {
+// 	queryString := request.URL.Query()
+// 	id, _ := strconv.ParseInt(queryString.Get("id"), 10, 64)
+	
+// 	pasienModel.Delete(id)
+// 	http.Redirect(response, request, "/pasien", http.StatusSeeOther)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
